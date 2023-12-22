8fe1e6d4abd42de92fad72a18744f8ff72cfe52b
leo <1027608831@qq.com>
2023-12-22 17:11:31 +0800

    树节点value改成val

--- src/msb/rookie/binaryTree/BinaryTreeTraversal.java
+++ src/msb/rookie/binaryTree/BinaryTreeTraversal.java
@@ -3,36 +3,51 @@ package msb.rookie.binaryTree;
 /**
  * @author leo
  * @version 1.0
- * @description: TODO
+ * @description: TODO 二叉树的遍历
  * @date 2023/4/14 00:30
  */
 public class BinaryTreeTraversal {
     StringBuffer sb = new StringBuffer();
 
+    /**
+     * 先序
+     *
+     * @param head
+     */
     public void preTravel(TreeNode head) {
         if (head == null) {
             return;
         }
-        sb.append(head.value + ';');
+        sb.append(head.val + ';');
         preTravel(head.left);
         preTravel(head.right);
     }
 
+    /**
+     * 中序
+     *
+     * @param head
+     */
     public void inTravel(TreeNode head) {
         if (head == null) {
             return;
         }
         inTravel(head.left);
-        sb.append(head.value + ';');
+        sb.append(head.val + ';');
         inTravel(head.right);
     }
 
+    /**
+     * 后序
+     *
+     * @param head
+     */
     public void postTravel(TreeNode head) {
         if (head == null) {
             return;
         }
         postTravel(head.left);
         postTravel(head.right);
-        sb.append(head.value + ';');
+        sb.append(head.val + ';');
     }
 }
--- src/msb/rookie/binaryTree/TreeNode.java
+++ src/msb/rookie/binaryTree/TreeNode.java
@@ -7,11 +7,20 @@ package msb.rookie.binaryTree;
  * @date 2023/4/14 00:14
  */
 public class TreeNode {
-    public int value;
+    public int val;
     public TreeNode left;
     public TreeNode right;
 
-    public TreeNode(int value) {
-        this.value = value;
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
